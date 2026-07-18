968326a6fc7886f43387263fd2b987778da41a44
Vignesh SB <vigneshsb403@gmail.com>
2026-07-18 17:27:28 +0530

    Initial Commit

---
+++ Loom/LoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoomApp.swift
+//  Loom
+//
+//  Created by Vignesh on 18/07/26.
+//
+
+import SwiftUI
+
+@main
+struct LoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
